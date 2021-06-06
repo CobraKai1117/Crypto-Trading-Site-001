dc5925c806c7d64dd82e7e7c5c227b7f00e94da6
CobraKai1117 <kennethrpaulsen@gmail.com>
2021-06-06 01:57:17 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Crypto_Trading_Site_001.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/UserInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crypto_Trading_Site_001.Models
+{
+    public class UserInfo:DbContext
+    {
+
+
+
+
+    }
+}
